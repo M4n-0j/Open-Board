efcfd80c3b7b7c926e3be6e4972f5ded9ddc76fb
M4n-0j <kumarmanoj.bz@gmail.com>
2023-04-25 19:21:21 +0530

    added functaionalities on sticky notes

--- tools.js
+++ tools.js
@@ -83,6 +83,53 @@ sticky.addEventListener("click", (e) => {
 </div>
     `;
     document.body.appendChild(stickyCont);
-
+let sCont=stickyCont;
+    let minimize=stickyCont.querySelector(".minimize");
+    let remove=stickyCont.querySelector(".remove");
+    noteActions(minimize,remove,sCont);
+    stickyCont.onmousedown = function (event) {
+        dragAndDrop(stickyCont, event);
+      };
+     
+      stickyCont.ondragstart = function() {
+        return false;
+      };
 
 })
+
+function noteActions(minimize,remove,stickyCont){
+   remove.addEventListener("click",(e)=>{
+    stickyCont.remove();
+   })  
+}
+
+function dragAndDrop(element,event){
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+  
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+   
+  
+    moveAt(event.pageX, event.pageY);
+  
+    // moves the ball at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+      element.style.left = pageX - shiftX + 'px';
+      element.style.top = pageY - shiftY + 'px';
+    }
+  
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // move the ball on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // drop the ball, remove unneeded handlers
+    element.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      element.onmouseup = null;
+    };
+}
